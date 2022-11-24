1167858b07b47d953ec2f7095ac6c65d747244ba
Diego Becco <diego.becco@etermax.com.uy>
2022-11-24 19:20:03 -0300

    When player one scores, 15-love

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -6,16 +6,30 @@ test('works', () => {
 });
 
 class TennisGame{
+    playerOneScore = 0;
+    playerTwoScore = 0;
     constructor(){
 
     }
 
+    wonPoint(player){
+        this.playerOneScore ++;
+    }
+
     getScore(){
-        return "Love-all"
+        if(this.playerOneScore == this.playerTwoScore)
+            return "Love-all"
+        return "15-Love"
     }
 }
 
 test('When a game has started, the score is Love-all',()=>{
     const game = new TennisGame()
     expect(game.getScore()).toBe("Love-all")
+})
+
+test('When player one scores on an empty game, score should be 15-Love',()=>{
+    const game = new TennisGame()
+    game.wonPoint("player1")
+    expect(game.getScore()).toBe("15-Love")
 })
